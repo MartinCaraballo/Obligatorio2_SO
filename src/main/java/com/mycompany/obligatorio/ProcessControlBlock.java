7958d33d0862d4f0f29071558166baf06cc893ab
Juan <juan.lupi@correo.ucu.edu.uy>
2022-06-02 16:15:13 -0300

    New classes added

---
+++ src/main/java/com/mycompany/obligatorio/ProcessControlBlock.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.obligatorio;
+import java.util.*;
+/**
+ *
+ * @author juan-
+ */
+public class ProcessControlBlock {
+    
+    // Lista de procesos suspendidos 
+    List<IProcess> suspendedProcess = new ArrayList<>();
+    
+    // Lista de procesos bloquados
+    List<IProcess> blockedProcess = new ArrayList<>();
+    
+    // Lista de procesos listos
+    List<IProcess> processList = new ArrayList<>();
+}
---
+++ src/main/java/com/mycompany/obligatorio/Stats.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.obligatorio;
+
+/**
+ *
+ * @author juan-
+ */
+public class Stats {
+    
+    // Porcentaje de CPU utilizada
+    public float collectCPUUsage; 
+    
+    // Porcentaje de RAM utilizada
+    public float collectRAMUsage;
+    
+    // Cantidad de procesos
+    public int collectProcessList; 
+    
+}
