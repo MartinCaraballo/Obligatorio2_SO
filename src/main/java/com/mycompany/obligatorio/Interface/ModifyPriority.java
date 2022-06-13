ebba7b458cd1e6ec34769683fba1b8c2f212e4c6
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2022-06-13 18:53:32 -0300

    updated drawio

--- src/main/java/com/mycompany/obligatorio/Interface/ModifyPriority.java
+++ src/main/java/com/mycompany/obligatorio/Interface/ModifyPriority.java
@@ -10,7 +10,7 @@ import javax.swing.*;
 
 public class ModifyPriority extends javax.swing.JFrame {
 
-    public static IProcess processToModify;
+    private static IProcess processToModify;
     
     public ModifyPriority() {
         initComponents();
